bd50d9c38de1ce4d3c74fd8a2165dae8980405ca
Tiago Almeida <herique.sa@gmail.com.com>
2020-11-06 15:55:30 -0300

    remove unnecessary spaces

--- crud_aluno_v1/src/services/http-common.js
+++ crud_aluno_v1/src/services/http-common.js
@@ -1,11 +1,5 @@
 import axios from 'axios';
-<<<<<<< HEAD
 const endPointAPI = process.env.VUE_APP_API_END_POINT;
-
-=======
-const endPointAPI = process.env.VUE_APP_API_END_POINT ;
->>>>>>> develop
-
 
 export default axios.create({
     baseURL: endPointAPI,
